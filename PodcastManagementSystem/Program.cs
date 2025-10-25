c9e35bb80ae7508288a40b9058e9573a8af5e922
Ohm Patel <ohm2003.patel@gmail.com>
2025-10-24 22:35:35 -0400

    Update project for AWS integration and configuration
    - Removed `appsettings.Development.json` and `appsettings.json` from `PodcastManagementSystem.csproj`.
    - Added AWS SDK package references, including `Amazon.Extensions.Configuration.SystemsManager`.
    - Introduced new folder `podcast\rds\` in the project structure.
    - Updated `Program.cs` with new using directives for AWS services.
    - Configured AWS Systems Manager for connection strings and credentials for RDS.
    - Implemented `SqlConnectionStringBuilder` for database connection string setup.
    - Added comments for future database context creation and SSMS setup.

--- PodcastManagementSystem/Program.cs
+++ PodcastManagementSystem/Program.cs
@@ -1,4 +1,7 @@
+using Amazon;
+using Amazon.Extensions.NETCore.Setup;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using PodcastManagementSystem.Data;
 
@@ -19,6 +22,24 @@ namespace PodcastManagementSystem
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+
+            builder.Configuration.AddSystemsManager("/podcast/rds", new AWSOptions
+            {
+                Region = RegionEndpoint.USEast2
+            });
+
+            var connectionStringParameter = new SqlConnectionStringBuilder(
+                builder.Configuration.GetConnectionString("Connection2RDS")
+            );
+            // Now these keys work because the prefix matches
+            connectionStringParameter.UserID = builder.Configuration["username"]; 
+            connectionStringParameter.Password = builder.Configuration["password"];
+
+            //STAGE 2 PENDING create the DB context and folders and DATABASE CREATION IN SSMS.
+            //connect first to the msql using explorer SQL
+            //builder.Services.AddDbContext<SMSContext>(options =>
+            //    options.UseSqlServer(connectionStringParameter.ConnectionString)
+            //);
 
             var app = builder.Build();
 
